85580e444910bfdf00f3b667b4b5cabeec6c9896
marctex <marcelo.t.martins@hotmail.com>
2018-03-06 08:39:12 -0300

    Versão 002

--- java/Ex13Garcia/src/Complexo.java
+++ java/Ex13Garcia/src/Complexo.java
@@ -1,4 +1,19 @@
 
 public class Complexo {
+	double aR;
+	double bR;
+	double aI;
+	double bI;
+	
+	public Complexo(double aR, double bR, double aI, double bI) {
+		this.aR = aR;
+		this.bR = bR;
+		this.aI = aI;
+		this.bI = bI;
+	}
+	
+	void soma() {
+		
+	}
 
 }
